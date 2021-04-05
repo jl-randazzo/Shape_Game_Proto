0c58f604ed22c018e8c57875b1a8e152c3842c55
jl-randazzo <jl_randazzo@hotmail.com>
2021-04-04 20:17:36 -0700

    update to include alphabet

---
+++ Assets/DialogueController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+
+    private DialogueController instance;
+
+    void Awake() {
+        if(instance == null) {
+            instance = new DialogueController();
+
+        } else
+        {
+
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
